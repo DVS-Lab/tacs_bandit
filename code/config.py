6fc8a865b08925b1948a95c0e8cf134c1bee7144
Jimmy Wyngaarden <james.wyngaarden@temple.edu>
2026-04-20 12:41:51 -0400

    update subs

--- code/config.py
+++ code/config.py
@@ -83,7 +83,8 @@ SUBJECT_INFO = {
     '10898': {'counterbalance': 'A', 'earclip': True, 'notes': ''},
     '10590': {'counterbalance': 'B', 'earclip': True, 'notes': ''},
     '10950': {'counterbalance': 'A', 'earclip': True, 'notes': ''},
-    '11036': {'counterbalance': 'B', 'earclip': True, 'notes': ''}
+    '11036': {'counterbalance': 'B', 'earclip': True, 'notes': ''},
+    '11031': {'counterbalance': 'A', 'earclip': True, 'notes': ''}
 }
 
 # Subjects without hardware earclip reference (first 4 participants)
